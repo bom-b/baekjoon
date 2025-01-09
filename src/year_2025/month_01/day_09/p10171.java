d7bf775cf1f87bb5287ba8dca6298c9eb72ca186
bom-b <sb97213@gmail.com>
2025-01-09 23:48:15 +0900

    20250109

---
+++ src/year_2025/month_01/day_09/p10171.java
@@ -0,0 +1,20 @@
+package year_2025.month_01.day_09;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class p10171 {
+    public static void solution() throws Exception{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\" + "\n");
+        bw.write(" )  ( ')" + "\n");
+        bw.write("(  /  )" + "\n");
+        bw.write(" \\(__)|");
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/year_2025/month_01/day_09/p10172.java
@@ -0,0 +1,18 @@
+package year_2025.month_01.day_09;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class p10172 {
+    public static void solution() throws Exception{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("|\\_/|" + "\n");
+        bw.write("|q p|   /}" + "\n");
+        bw.write("( 0 )\"\"\"\\" + "\n");
+        bw.write("|\"^\"`    |" + "\n");
+        bw.write("||_/=\\\\__|");
+        bw.flush();
+        bw.close();
+    }
+}
