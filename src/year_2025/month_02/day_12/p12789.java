795f825da71aa49fd151107f50c1db7d9c095bda
bom-b <sb97213@gmail.com>
2025-02-12 23:49:25 +0900

    20250212

--- src/year_2025/month_02/day_12/p12789.java
+++ src/year_2025/month_02/day_12/p12789.java
@@ -1,5 +1,43 @@
 package year_2025.month_02.day_12;
 
-public class p12789 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
+public class p12789 {
+    public static void solution() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            int N = Integer.parseInt(br.readLine());
+            int nextNum = 1;
+            Stack<Integer> stack = new Stack<>();
+            String result = "Nice";
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (N-- > 0) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num == nextNum) {
+                    nextNum++;
+                } else {
+                    while (!stack.isEmpty() && stack.peek() == nextNum) {
+                        nextNum++;
+                        stack.pop();
+                    }
+
+                    if (stack.isEmpty() || stack.peek() > num) {
+                        stack.push(num);
+                    } else {
+                        result = "Sad";
+                        break;
+                    }
+                }
+            }
+
+            bw.write(result);
+        }
+    }
 }
