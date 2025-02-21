533ccf5c4a1014bc1d90d15e418727b9636dce8c
bom-b <sb97213@gmail.com>
2025-02-21 23:28:38 +0900

    20250221

---
+++ src/year_2025/month_02/day_21/p10189.java
@@ -0,0 +1,16 @@
+package year_2025.month_02.day_21;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class p10189 {
+    public static void solution() throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            bw.write("#  # #### #### #  #" + "\n");
+            bw.write("#### #  # #  # # #" + "\n");
+            bw.write("#### #  # #  # # #" + "\n");
+            bw.write("#  # #### #### #  #");
+        }
+    }
+}
