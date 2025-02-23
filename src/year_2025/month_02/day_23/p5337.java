a12978cf69ccd26af931416eadab852ba75d522e
bom-b <sb97213@gmail.com>
2025-02-23 23:51:13 +0900

    20250223

---
+++ src/year_2025/month_02/day_23/p5337.java
@@ -0,0 +1,15 @@
+package year_2025.month_02.day_23;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class p5337 {
+    public static void solution() throws Exception {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            bw.write(".  .   ." + "\n");
+            bw.write("|  | _ | _. _ ._ _  _" + "\n");
+            bw.write("|/\\|(/.|(_.(_)[ | )(/.");
+        }
+    }
+}
