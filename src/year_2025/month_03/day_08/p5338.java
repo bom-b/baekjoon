56e203b3683b65afd688fb45e979d614228f4414
bom-b <sb97213@gmail.com>
2025-03-08 15:09:43 +0900

    20250308

---
+++ src/year_2025/month_03/day_08/p5338.java
@@ -0,0 +1,20 @@
+package year_2025.month_03.day_08;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p5338 {
+    public static void solution() throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            bw.write("       _.-;;-._" + "\n");
+            bw.write("'-..-'|   ||   |" + "\n");
+            bw.write("'-..-'|_.-;;-._|" + "\n");
+            bw.write("'-..-'|   ||   |" + "\n");
+            bw.write("'-..-'|_.-''-._|" + "\n");
+        }
+    }
+}
